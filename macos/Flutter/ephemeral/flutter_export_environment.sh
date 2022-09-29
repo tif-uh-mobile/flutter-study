081926a78940ccafc9b31f3cecbf23e3a6a4b88b
christiechindy <88538204+christiechindy@users.noreply.github.com>
2022-09-29 13:09:46 +0800

    Tugas Flutter 1

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\ikhsa\OneDrive\Dokumen\Flutter Project\one"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\LENOVO\Documents\flutter_learn_basics"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
