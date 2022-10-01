131412b3bc22d8cbc09c902d7909627f0513a1e6
faizalfajri66 <81213452+faizalfajri66@users.noreply.github.com>
2022-10-01 15:22:36 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tif_flutter/nim_rahmat/nim_profile_screen.dart';
+import 'package:tif_flutter/D121201012_Muh_Faisal_Fajri/D121201012_profile_screen.dart';
 
 void main() {
   runApp(const MyApp());
