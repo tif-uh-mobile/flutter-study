b360a672d503d7711b2fd7539047ccb9dc8c6d2c
gabrield-droid <gabrieldylan474@gmail.com>
2022-10-03 15:51:19 +0800

    D121201025_Gabriel Dylan

---
+++ lib/D121201025_dylan/d121201025_profile_screen.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+class NimProfileScreen extends StatefulWidget {
+  const NimProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  State<NimProfileScreen> createState() => _NimProfileScreenState();
+}
+
+class _NimProfileScreenState extends State<NimProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('D121201025 Gabriel Dylan Profile Screen'),
+      ),
+      body: Column(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 5, left: 32),
+                width: 100.0,
+                height: 130.0,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.contain,
+                        image: AssetImage('assets/images/Untitled31.png'))),
+              ),
+              Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(
+                      left: 20,
+                    ),
+                    width: 220,
+                    height: 40,
+                    decoration:
+                        BoxDecoration(border: Border.all(color: Colors.grey)),
+                    child: Center(
+                      child: Text(
+                        "Gabriel Dylan",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 20, top: 5, bottom: 5),
+                    width: 220,
+                    height: 25,
+                    decoration:
+                        BoxDecoration(border: Border.all(color: Colors.grey)),
+                    child: Center(
+                      child: Text(
+                        "Membaca",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 20, top: 15, bottom: 5, right: 15),
+            width: 340,
+            height: 30,
+            decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+            child: Center(
+              child: Text(
+                "Universitas Hasanuddin",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 20, top: 15, bottom: 5, right: 15),
+            padding: EdgeInsets.all(8),
+            width: 340,
+            height: 500,
+            decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+            child: Text(
+              "Seorang mahasiswa Teknik Informatika di Universitas Hasanuddin",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tif_flutter/D121201025_dylan/d121201025_profile_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'My Profile'),
     );
   }
 }
@@ -29,7 +30,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,23 +38,88 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
           child: Column(
-        children: [],
+        // children: const [
+        //  NavigateButton(
+        //      name: 'D121201025-Gabriel Dylan', page: NimProfileScreen())
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 32),
+            width: 190.0,
+            height: 190.0,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage('assets/images/Untitled31.png'))),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Container(
+            width: 250,
+            height: 60,
+            decoration: BoxDecoration(color: Colors.grey),
+            child: Center(
+              child: Text(
+                "Gabriel Dylan Valentino",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 12),
+            width: 250,
+            height: 60,
+            decoration: BoxDecoration(color: Colors.grey),
+            child: Center(
+              child: Text(
+                "Membaca",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 12, bottom: 20),
+            width: 250,
+            height: 60,
+            decoration: BoxDecoration(color: Colors.grey),
+            child: Center(
+              child: Text(
+                "Abu-abu",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          NavigateButton(
+              name: 'D121201025-Gabriel Dylan', page: NimProfileScreen())
+        ],
       )),
     );
   }
 }
 
 class NavigateButton extends StatelessWidget {
-
-  const NavigateButton({Key? key, required this.name, required  this.page}) : super(key: key);
+  const NavigateButton({Key? key, required this.name, required this.page})
+      : super(key: key);
 
   final Widget page;
   final String name;
 
-
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton (
+    return ElevatedButton(
       child: Text(name),
       onPressed: () {
         Navigator.push(context, MaterialPageRoute(builder: (ctx) => page));
@@ -62,4 +127,3 @@ class NavigateButton extends StatelessWidget {
     );
   }
 }
-
